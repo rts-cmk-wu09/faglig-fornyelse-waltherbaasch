3d1180a3ecba2d2d5d0183e2cc7758a42b070d09
waltherbaasch <U2444127@rts-365.dk>
2023-08-08 10:37:22 +0200

    added tailwind

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,22 +16,30 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Football Player Stats App</h1>
-      <input
-        type="text"
-        placeholder="Enter player name"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      <button onClick={handleSearch}>Search</button>
+    <div className="bg-gray-100 min-h-screen p-8">
+      <h1 className="text-2xl font-semibold mb-4">Football Player Stats App</h1>
+      <div className="flex mb-4">
+        <input
+          type="text"
+          placeholder="Enter player name"
+          className="border border-gray-300 rounded px-4 py-2 w-full"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button
+          className="bg-blue-500 text-white rounded px-4 py-2 ml-4"
+          onClick={handleSearch}
+        >
+          Search
+        </button>
+      </div>
 
       {playerStats && (
-        <div>
-          <h2>Player Stats for {playerStats.name}</h2>
+        <div className="border border-gray-300 p-4 rounded">
+          <h2 className="text-lg font-semibold">Player Stats for {playerStats.name}</h2>
           <p>Goals: {playerStats.goals}</p>
           <p>Assists: {playerStats.assists}</p>
-          {/* Flere stats her */}
+          {/* flere stats*/}
         </div>
       )}
     </div>
@@ -39,4 +47,5 @@ function App() {
 }
 
 export default App;
+
 
